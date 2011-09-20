35def6bcd3adaeea39d1a06b004143d46d13ec06
darek <darek@dapago.net>
2011-09-20 10:13:05 +0000

    first commit

---
+++ classes/DpdMessages.php
@@ -0,0 +1,16 @@
+<?php
+class DpdMessages {
+    /*
+     * todo
+     */
+    public static $error = array(
+        "pl_PL" => array(
+            "1" => "Host musi być poprawnie uzupełniony"
+        ),
+        "en_EN" => array(
+            "1" => "Host address must be set",
+        )
+    );
+    
+}
+?>
